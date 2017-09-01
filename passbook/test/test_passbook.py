f8c82753cd51fc50f4d39c4434d3515c6d9de320
Martin Bächtold <martin@baechtold.me>
2017-09-01 23:23:21 +0200

    Add support for Python 3.

--- passbook/test/test_passbook.py
+++ passbook/test/test_passbook.py
@@ -1,16 +1,12 @@
-"""
-Test some basic pass file generation
-"""
+# -*- coding: utf-8 -*-
+import json
 
-try:
-    import json
-except ImportError:
-    import simplejson as json
 import pytest
 from M2Crypto import BIO
 from M2Crypto import SMIME
 from M2Crypto import X509
-from M2Crypto import m2
+from future.builtins import bytes
+from future.builtins import str
 from path import Path
 
 from passbook.models import Barcode, BarcodeFormat, Pass, StoreCard
@@ -150,6 +146,7 @@ def test_signing():
     them to git. Store them in the files indicated below, they are ignored
     by git.
     """
+    from io import open
     try:
         with open(password_file) as file_:
             password = file_.read().strip()
@@ -158,7 +155,7 @@ def test_signing():
 
     passfile = create_shell_pass()
     manifest_json = passfile._createManifest(passfile._createPassJson())
-    signature = passfile._createSignature(
+    signature = passfile._sign_manifest(
         manifest_json,
         certificate,
         key,
@@ -166,30 +163,31 @@ def test_signing():
         password,
     )
 
-    smime_obj = SMIME.SMIME()
+    smime = passfile._get_smime(
+        certificate,
+        key,
+        wwdr_certificate,
+        password,
+    )
 
     store = X509.X509_Store()
-    store.load_info(str(wwdr_certificate))
-    smime_obj.set_x509_store(store)
+    try:
+        store.load_info(bytes(wwdr_certificate, encoding='utf8'))
+    except TypeError:
+        store.load_info(str(wwdr_certificate))
 
-    signature_bio = BIO.MemoryBuffer(signature)
-    signature_p7 = SMIME.PKCS7(m2.pkcs7_read_bio_der(signature_bio._ptr()), 1)
+    smime.set_x509_store(store)
 
-    stack = signature_p7.get0_signers(X509.X509_Stack())
-    smime_obj.set_x509_stack(stack)
-
-    data_bio = BIO.MemoryBuffer(manifest_json)
+    data_bio = BIO.MemoryBuffer(bytes(manifest_json, encoding='utf8'))
 
     # PKCS7_NOVERIFY = do not verify the signers certificate of a signed message.
-    assert smime_obj.verify(
-        signature_p7, data_bio, flags=SMIME.PKCS7_NOVERIFY
-    ) == manifest_json
+    assert smime.verify(signature, data_bio, flags=SMIME.PKCS7_NOVERIFY) == bytes(manifest_json, encoding='utf8')
 
-    tampered_manifest = '{"pass.json": "foobar"}'
+    tampered_manifest = bytes('{"pass.json": "foobar"}', encoding='utf8')
     data_bio = BIO.MemoryBuffer(tampered_manifest)
     # Verification MUST fail!
     with pytest.raises(SMIME.PKCS7_Error):
-        smime_obj.verify(signature_p7, data_bio, flags=SMIME.PKCS7_NOVERIFY)
+        smime.verify(signature, data_bio, flags=SMIME.PKCS7_NOVERIFY)
 
 
 @pytest.mark.skipif(_certificates_mising(), reason='Certificates missing')
@@ -200,6 +198,7 @@ def test_passbook_creation():
     them to git. Store them in the files indicated below, they are ignored
     by git.
     """
+    from io import open
     try:
         with open(password_file) as file_:
             password = file_.read().strip()
@@ -207,5 +206,5 @@ def test_passbook_creation():
         password = ''
 
     passfile = create_shell_pass()
-    passfile.addFile('icon.png', open(cwd / 'static/white_square.png', 'rb'))
+    passfile.addFile('icon.png', open(cwd / 'static' / 'white_square.png', 'rb'))
     passfile.create(certificate, key, wwdr_certificate, password)
